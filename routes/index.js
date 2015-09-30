11f9f7cc132723d6f06fab453678eac296f7dc1d
Frode Danielsen <frode@e5r.no>
2015-09-30 18:10:13 +0200

    Initial form handling and confirmation page

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,39 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res/*, next*/) {
     res.render('index', { title: 'Døråpner' });
+});
+
+router.post('/', function (req, res/*, next*/) {
+    var errors = {};
+    if (!req.body.phone) {
+        errors.phone = 'Må fylles ut';
+    }
+    if (!req.body.code) {
+        errors.code = 'Må fylles ut';
+    }
+
+    if (req.body.code === '3146') {
+        // TODO: Logging
+        res.redirect('/confirmation');
+        return;
+    }
+    else if (!errors.code) {
+        errors.code = 'Ikke gyldig';
+    }
+
+    res.render('index', {
+        title: 'Døråpner',
+        data: req.body,
+        errors: errors
+    });
+});
+
+router.get('/confirmation', function (req, res/*, next*/) {
+    res.render('confirmation', {
+        title: 'Velkommen inn'
+    });
 });
 
 module.exports = router;
