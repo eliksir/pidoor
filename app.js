f95837d7264309910aa2bb9f4049db994187b785
Frode Danielsen <frode@e5r.no>
2015-09-30 19:06:00 +0200

    Fix dependencies, middleware and logging for prod

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 
 var app = express();
+var isDev = app.get('env') === 'development';
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -15,15 +16,18 @@ app.set('view engine', 'hbs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(logger(isDev ? 'dev' : 'combined'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(require('node-sass-middleware')({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  sourceMap: true
-}));
+
+if (isDev) {
+    app.use(require('node-sass-middleware')({
+      src: path.join(__dirname, 'public'),
+      dest: path.join(__dirname, 'public'),
+      sourceMap: true
+    }));
+}
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
@@ -39,7 +43,7 @@ app.use(function(req, res, next) {
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
+if (isDev) {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
